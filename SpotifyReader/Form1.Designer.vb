4bdf76737de67be1f750e793fc93dffda399ef92
blogcraft <blog_craft@outlook.com>
2014-07-21 04:35:25 -0400

    Seagregan formularios faltantes

---
+++ SpotifyReader/Form1.Designer.vb
@@ -0,0 +1,192 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BtnBuscar = New System.Windows.Forms.Button()
+        Me.TboxBusqueda = New System.Windows.Forms.TextBox()
+        Me.DgvBusqueda = New System.Windows.Forms.DataGridView()
+        Me.BtnGuardar = New System.Windows.Forms.Button()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.DgvResultadoSp = New System.Windows.Forms.DataGridView()
+        Me.DgvArtistasGuardados = New System.Windows.Forms.DataGridView()
+        CType(Me.DgvBusqueda, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        CType(Me.DgvResultadoSp, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DgvArtistasGuardados, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'BtnBuscar
+        '
+        Me.BtnBuscar.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnBuscar.Location = New System.Drawing.Point(643, 5)
+        Me.BtnBuscar.Name = "BtnBuscar"
+        Me.BtnBuscar.Size = New System.Drawing.Size(75, 23)
+        Me.BtnBuscar.TabIndex = 0
+        Me.BtnBuscar.Text = "Buscar"
+        Me.BtnBuscar.UseVisualStyleBackColor = True
+        '
+        'TboxBusqueda
+        '
+        Me.TboxBusqueda.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TboxBusqueda.Location = New System.Drawing.Point(6, 7)
+        Me.TboxBusqueda.Name = "TboxBusqueda"
+        Me.TboxBusqueda.Size = New System.Drawing.Size(631, 20)
+        Me.TboxBusqueda.TabIndex = 1
+        '
+        'DgvBusqueda
+        '
+        Me.DgvBusqueda.AllowUserToAddRows = False
+        Me.DgvBusqueda.AllowUserToDeleteRows = False
+        Me.DgvBusqueda.AllowUserToResizeRows = False
+        Me.DgvBusqueda.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.DgvBusqueda.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DgvBusqueda.Location = New System.Drawing.Point(6, 33)
+        Me.DgvBusqueda.MultiSelect = False
+        Me.DgvBusqueda.Name = "DgvBusqueda"
+        Me.DgvBusqueda.ReadOnly = True
+        Me.DgvBusqueda.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders
+        Me.DgvBusqueda.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.DgvBusqueda.Size = New System.Drawing.Size(712, 290)
+        Me.DgvBusqueda.TabIndex = 2
+        '
+        'BtnGuardar
+        '
+        Me.BtnGuardar.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnGuardar.Location = New System.Drawing.Point(6, 329)
+        Me.BtnGuardar.Name = "BtnGuardar"
+        Me.BtnGuardar.Size = New System.Drawing.Size(712, 23)
+        Me.BtnGuardar.TabIndex = 3
+        Me.BtnGuardar.Text = "Guardar"
+        Me.BtnGuardar.UseVisualStyleBackColor = True
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Location = New System.Drawing.Point(12, 12)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(732, 384)
+        Me.TabControl1.TabIndex = 4
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.DgvBusqueda)
+        Me.TabPage1.Controls.Add(Me.BtnGuardar)
+        Me.TabPage1.Controls.Add(Me.BtnBuscar)
+        Me.TabPage1.Controls.Add(Me.TboxBusqueda)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(724, 358)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Busqueda"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.DgvResultadoSp)
+        Me.TabPage2.Controls.Add(Me.DgvArtistasGuardados)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(724, 358)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Mis artistas"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'DgvResultadoSp
+        '
+        Me.DgvResultadoSp.AllowUserToAddRows = False
+        Me.DgvResultadoSp.AllowUserToDeleteRows = False
+        Me.DgvResultadoSp.AllowUserToResizeRows = False
+        Me.DgvResultadoSp.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.DgvResultadoSp.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DgvResultadoSp.Location = New System.Drawing.Point(237, 6)
+        Me.DgvResultadoSp.MultiSelect = False
+        Me.DgvResultadoSp.Name = "DgvResultadoSp"
+        Me.DgvResultadoSp.ReadOnly = True
+        Me.DgvResultadoSp.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders
+        Me.DgvResultadoSp.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.DgvResultadoSp.Size = New System.Drawing.Size(481, 346)
+        Me.DgvResultadoSp.TabIndex = 4
+        '
+        'DgvArtistasGuardados
+        '
+        Me.DgvArtistasGuardados.AllowUserToAddRows = False
+        Me.DgvArtistasGuardados.AllowUserToDeleteRows = False
+        Me.DgvArtistasGuardados.AllowUserToResizeRows = False
+        Me.DgvArtistasGuardados.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.DgvArtistasGuardados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DgvArtistasGuardados.Location = New System.Drawing.Point(6, 6)
+        Me.DgvArtistasGuardados.MultiSelect = False
+        Me.DgvArtistasGuardados.Name = "DgvArtistasGuardados"
+        Me.DgvArtistasGuardados.ReadOnly = True
+        Me.DgvArtistasGuardados.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders
+        Me.DgvArtistasGuardados.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.DgvArtistasGuardados.Size = New System.Drawing.Size(225, 346)
+        Me.DgvArtistasGuardados.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(756, 408)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Name = "Form1"
+        Me.Text = "Spotify Reader"
+        CType(Me.DgvBusqueda, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
+        Me.TabPage2.ResumeLayout(False)
+        CType(Me.DgvResultadoSp, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DgvArtistasGuardados, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents BtnBuscar As System.Windows.Forms.Button
+    Friend WithEvents TboxBusqueda As System.Windows.Forms.TextBox
+    Friend WithEvents DgvBusqueda As System.Windows.Forms.DataGridView
+    Friend WithEvents BtnGuardar As System.Windows.Forms.Button
+    Friend WithEvents TabControl1 As System.Windows.Forms.TabControl
+    Friend WithEvents TabPage1 As System.Windows.Forms.TabPage
+    Friend WithEvents TabPage2 As System.Windows.Forms.TabPage
+    Friend WithEvents DgvArtistasGuardados As System.Windows.Forms.DataGridView
+    Friend WithEvents DgvResultadoSp As System.Windows.Forms.DataGridView
+
+End Class
---
+++ SpotifyReader/Form1.vb
@@ -0,0 +1,283 @@
+﻿Imports System.Net
+Imports System.IO
+Imports Newtonsoft.Json
+Imports System.Data.SqlClient
+
+Public Class Form1
+
+    Private Sub BtnBuscar_Click(sender As Object, e As EventArgs) Handles BtnBuscar.Click
+        Dim tipoConsulta As String
+
+        tipoConsulta = "artist"
+
+        Dim responseFromServer As String
+        responseFromServer = SpotifyRequest("https://api.spotify.com/v1/search?q=" & TboxBusqueda.Text & "&type=" & tipoConsulta)
+
+        Dim objeto As ArtistQuery
+        objeto = JsonConvert.DeserializeObject(Of ArtistQuery)(responseFromServer)
+
+        Dim dt As New DataTable
+        dt.TableName = "Artists"
+        dt.Columns.Add("id", Type.GetType("System.String"))
+        dt.Columns.Add("name", Type.GetType("System.String"))
+        'dt.Columns.Add("popularity", Type.GetType("System.String"))
+        'dt.Columns.Add("uri", Type.GetType("System.String"))
+
+        For Each artist In objeto.artist.item
+            Dim dr As DataRow
+            dr = dt.NewRow
+            dr("id") = artist.id
+            dr("name") = artist.name
+            'dr("popularity") = artist.popularity
+            'dr("uri") = artist.uri
+            dt.Rows.Add(dr)
+        Next (artist)
+
+        Dim ds As New DataSet
+        ds.Tables.Add(dt)
+
+        DgvBusqueda.AutoGenerateColumns = True
+        DgvBusqueda.DataMember = "Artists"
+        DgvBusqueda.DataSource = ds
+        DgvBusqueda.Columns(0).Visible = False
+        DgvBusqueda.Columns(1).HeaderText = "Artista"
+    End Sub
+
+    Private Sub BtnGuardar_Click(sender As Object, e As EventArgs) Handles BtnGuardar.Click
+        Dim ds As DataSet
+        Dim tabla As String
+        ds = DgvBusqueda.DataSource
+        tabla = "Artists"
+
+        Cursor.Current = Cursors.WaitCursor
+        For Each drSearch As DataRow In ds.Tables(tabla).Rows
+            If Not existe(drSearch("id"), tabla) Then
+                Dim responseFromServer As String
+                responseFromServer = SpotifyRequest("https://api.spotify.com/v1/artists/" & drSearch("id"))
+
+                Dim objArtist As Artist
+                objArtist = JsonConvert.DeserializeObject(Of Artist)(responseFromServer)
+
+                Dim dtArtist As New DataTable
+                dtArtist.TableName = "Artists"
+                dtArtist.Columns.Add("id", Type.GetType("System.String"))
+                dtArtist.Columns.Add("name", Type.GetType("System.String"))
+                dtArtist.Columns.Add("popularity", Type.GetType("System.String"))
+                dtArtist.Columns.Add("uri", Type.GetType("System.String"))
+
+                Dim drArtist As DataRow
+                drArtist = dtArtist.NewRow
+                drArtist("id") = objArtist.id
+                drArtist("name") = objArtist.name
+                drArtist("popularity") = objArtist.popularity
+                drArtist("uri") = objArtist.uri
+
+                guardar(drArtist, tabla)
+
+                responseFromServer = SpotifyRequest("https://api.spotify.com/v1/artists/" & drSearch("id") & "/albums")
+
+                Dim albums As AlbumQuery
+                albums = JsonConvert.DeserializeObject(Of AlbumQuery)(responseFromServer)
+
+                For Each album In albums.item
+                    If Not existe(drSearch("id"), "Albums") Then
+                        responseFromServer = SpotifyRequest("https://api.spotify.com/v1/albums/" & album.id)
+                        Dim objAlbum As Album
+                        objAlbum = JsonConvert.DeserializeObject(Of Album)(responseFromServer)
+
+                        Dim dtAlbum As New DataTable
+                        dtAlbum.TableName = "Albums"
+                        dtAlbum.Columns.Add("id", Type.GetType("System.String"))
+                        dtAlbum.Columns.Add("name", Type.GetType("System.String"))
+                        dtAlbum.Columns.Add("uri", Type.GetType("System.String"))
+                        dtAlbum.Columns.Add("popularity", Type.GetType("System.Int32"))
+                        dtAlbum.Columns.Add("release_date", Type.GetType("System.String"))
+                        dtAlbum.Columns.Add("artist_id", Type.GetType("System.String"))
+
+                        Dim drAlbum As DataRow
+                        drAlbum = dtAlbum.NewRow
+                        drAlbum("id") = objAlbum.id
+                        drAlbum("name") = objAlbum.name
+                        drAlbum("uri") = objAlbum.uri
+                        drAlbum("popularity") = objAlbum.popularity
+                        drAlbum("release_date") = objAlbum.release_date
+                        drAlbum("artist_id") = objArtist.id
+                        guardar(drAlbum, "Albums")
+
+                    End If
+
+                    responseFromServer = SpotifyRequest("https://api.spotify.com/v1/albums/" & album.id & "/tracks")
+                    Dim Tracks As TrackQuery
+                    Tracks = JsonConvert.DeserializeObject(Of TrackQuery)(responseFromServer)
+
+                    Dim dtTrack As New DataTable
+                    dtTrack.TableName = "Tracks"
+                    dtTrack.Columns.Add("id", Type.GetType("System.String"))
+                    dtTrack.Columns.Add("name", Type.GetType("System.String"))
+                    dtTrack.Columns.Add("uri", Type.GetType("System.String"))
+                    dtTrack.Columns.Add("duration_ms", Type.GetType("System.Int32"))
+                    dtTrack.Columns.Add("album_id", Type.GetType("System.String"))
+                    dtTrack.Columns.Add("popularity", Type.GetType("System.Int32"))
+
+                    For Each track In Tracks.item
+                        Dim drAlbum As DataRow
+                        drAlbum = dtTrack.NewRow
+                        drAlbum("id") = track.id
+                        drAlbum("name") = track.name
+                        drAlbum("uri") = track.uri
+                        drAlbum("duration_ms") = track.duration_ms
+                        drAlbum("album_id") = album.id
+                        drAlbum("popularity") = track.popularity
+                        If Not existe(drSearch("id"), "Tracks") Then
+                            guardar(drAlbum, "Tracks")
+                        End If
+                    Next
+                Next
+            End If
+        Next
+        Cursor.Current = Cursors.Default
+        MsgBox("Operacion Finalizada")
+    End Sub
+    Private Function existe(ByVal id As String, ByVal tabla As String) As Boolean
+        Dim resultado As Boolean
+
+        Dim myconnect As New SqlClient.SqlConnection
+        Dim mycommand As SqlCommand = New SqlCommand()
+
+        myconnect.ConnectionString = "Server=(localdb)\v11.0;Integrated Security=true;AttachDbFilename=|DataDirectory|\SpotifyDB.mdf"
+        mycommand.Connection = myconnect
+        mycommand.CommandText = "SELECT * FROM " & tabla & " WHERE id = @id"
+        myconnect.Open()
+
+        mycommand.Parameters.AddWithValue("@id", id)
+        Using reader As SqlDataReader = mycommand.ExecuteReader()
+            If reader.HasRows Then
+                resultado = True
+            Else
+                resultado = False
+            End If
+        End Using
+        myconnect.Close()
+        Return resultado
+    End Function
+
+    Private Function guardar(ByVal dr As DataRow, ByVal tabla As String) As Boolean
+        Dim myconnect As New SqlClient.SqlConnection
+        Dim mycommand As SqlCommand = New SqlCommand()
+        Dim columnas As String
+
+        If tabla = "Artists" Then
+            columnas = " (id, name, popularity, uri) VALUES (@id, @name, @popularity, @uri)"
+        ElseIf tabla = "Tracks" Then
+            columnas = " (id, name, popularity, uri, duration_ms, album_id) VALUES (@id, @name, @popularity, @uri, @duration_ms, @album_id)"
+        Else
+            columnas = " (id, name, popularity, uri, release_date, artist_id) VALUES (@id, @name, @popularity, @uri, @release_date, @artist_id)"
+        End If
+
+        myconnect.ConnectionString = "Server=(localdb)\v11.0;Integrated Security=true;AttachDbFilename=|DataDirectory|\SpotifyDB.mdf"
+        mycommand.Connection = myconnect
+        mycommand.CommandText = "INSERT INTO " & tabla & columnas
+        myconnect.Open()
+
+        Try
+            mycommand.Parameters.Add("@ID", SqlDbType.NVarChar).Value = dr("id")
+            mycommand.Parameters.Add("@name", SqlDbType.NVarChar).Value = dr("name")
+            mycommand.Parameters.Add("@popularity", SqlDbType.Int).Value = dr("popularity")
+            mycommand.Parameters.Add("@uri", SqlDbType.NVarChar).Value = dr("uri")
+            If tabla = "Tracks" Then
+                mycommand.Parameters.Add("@duration_ms", SqlDbType.Int).Value = dr("duration_ms")
+                mycommand.Parameters.Add("@album_id", SqlDbType.NVarChar).Value = dr("album_id")
+            End If
+            If tabla = "Albums" Then
+                mycommand.Parameters.Add("@release_date", SqlDbType.NVarChar).Value = dr("release_date").ToString.Substring(0, 4)
+                mycommand.Parameters.Add("@artist_id", SqlDbType.NVarChar).Value = dr("artist_id")
+            End If
+            mycommand.ExecuteNonQuery()
+            Return True
+        Catch ex As SqlException
+            Cursor.Current = Cursors.Default
+            MsgBox(tabla & " " & ex.Message)
+            Return False
+        End Try
+        myconnect.Close()
+        Return True
+    End Function
+
+    Public Function SpotifyRequest(URL As String) As String
+        Dim request As WebRequest = WebRequest.Create(URL)
+        ' If required by the server, set the credentials.
+        request.Credentials = CredentialCache.DefaultCredentials
+
+        ' Get the response. 
+        Dim response As HttpWebResponse = CType(request.GetResponse(), HttpWebResponse)
+        ' Display the status.
+        Console.WriteLine(response.StatusDescription)
+        ' Get the stream containing content returned by the server. 
+        Dim dataStream As Stream = response.GetResponseStream()
+        ' Open the stream using a StreamReader for easy access. 
+        Dim reader As New StreamReader(dataStream)
+        ' Read the content. 
+        Dim responseFromServer As String = reader.ReadToEnd()
+        ' Display the content.
+        Console.WriteLine(responseFromServer)
+        ' Cleanup the streams and the response.
+        reader.Close()
+        dataStream.Close()
+        response.Close()
+
+        Return responseFromServer
+    End Function
+
+    Private Sub TabControl1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles TabControl1.SelectedIndexChanged
+
+        Dim tabla As String
+        Dim myconnect As New SqlClient.SqlConnection
+        Dim mycommand As SqlCommand = New SqlCommand()
+
+
+        tabla = "Artists"
+        myconnect.ConnectionString = "Server=(localdb)\v11.0;Integrated Security=true;AttachDbFilename=|DataDirectory|\SpotifyDB.mdf"
+        myconnect.Open()
+        mycommand.Connection = myconnect
+        mycommand.CommandText = "SELECT Id, name FROM " & tabla
+        Dim myadapter As New SqlDataAdapter(mycommand)
+        Dim ds As New DataSet
+        myadapter.Fill(ds, tabla)
+
+        myconnect.Close()
+
+        DgvArtistasGuardados.AutoGenerateColumns = True
+        DgvArtistasGuardados.DataMember = "Artists"
+        DgvArtistasGuardados.DataSource = ds
+        DgvArtistasGuardados.Columns(0).Visible = False
+        DgvArtistasGuardados.Columns(1).HeaderText = "Artista"
+        DgvArtistasGuardados.Columns(1).Width = 100
+    End Sub
+
+    Private Sub DgvArtistasGuardados_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DgvArtistasGuardados.CellContentClick
+        Dim tabla As String
+        Dim myconnect As New SqlClient.SqlConnection
+        Dim mycommand As SqlCommand = New SqlCommand()
+
+
+        tabla = "Artists"
+        myconnect.ConnectionString = "Server=(localdb)\v11.0;Integrated Security=true;AttachDbFilename=|DataDirectory|\SpotifyDB.mdf"
+        myconnect.Open()
+        mycommand.Connection = myconnect
+        mycommand.CommandText = "exec ConsultaPorArtista @Id='" & DgvArtistasGuardados.Rows(e.RowIndex).Cells(0).Value & "'"
+        Dim myadapter As New SqlDataAdapter(mycommand)
+        Dim ds As New DataSet
+        myadapter.Fill(ds, tabla)
+
+        myconnect.Close()
+
+        DgvResultadoSp.AutoGenerateColumns = True
+        DgvResultadoSp.DataMember = "Artists"
+        DgvResultadoSp.DataSource = ds
+        DgvResultadoSp.Columns(0).HeaderText = "Album"
+        DgvResultadoSp.Columns(1).HeaderText = "Fecha de Lanzamiento"
+        DgvResultadoSp.Columns(2).HeaderText = "Popularidad Promedio"
+        DgvResultadoSp.Columns(3).HeaderText = "Canción Más Larga"
+        DgvResultadoSp.Columns(4).HeaderText = "Duración de Canción Más Larga"
+    End Sub
+End Class
